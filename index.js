10d7ad9475cc360d2f6e5b8298b6eb5ab7d4ced0
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2021-10-30 00:18:42 -0500

    Upload single image feature

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-// ACTION: Import the Express library into our project. Path library provides a utility for working with file and 
-// directory paths. The less-middlewear library allows us to process "less" files as required by the Express.JS 
-// framework.
-var express = require('express');
-var path = require('path');
-var less = require('less-middleware');
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const less = require('less-middleware');
 
-// SERVER VERSION #1 ---------- ----------
+const helpers = require('./helpers');
+const port = process.env.PORT || 5000;
+const app = express();
 
-// ACTION: Create an instance of our Express server named myApp.
-var myApp = express();
-    
-myApp.get('/', function(requestA, responseB) {
-	responseB.send('Success');
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-// SERVER VERSION #2 ---------- ----------
-
-	
-
-// ACTION: Use this template available online from: 
-// https://blackbricksoftware.com/bit-on-bytes/77-basic-node-js-web-server-setup
-myApp.use(less(path.join(__dirname,'source','less'),{
-    dest: path.join(__dirname, 'public'),
-    options: {
-        compiler: {
-            compress: false,
-        },
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/uploads/');
     },
-    preprocess: {
-        path: function(pathname, req) {
-            return pathname.replace('/css/','/'); 
-        },
-    },
-    force: true,
-}));
 
-// ACTION: Define static server.
-myApp.use(express.static(path.join(__dirname, 'public')));
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+var upload = multer({ storage: storage, fileFilter: helpers.imageFilter })
+
+app.post('/upload-single', upload.single('profile_pic'), (req, res) => {
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        res.send(`You have uploaded this image: <hr/><img src="./uploads/${req.file.filename}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
 
 
-// BEGIN LISTENING MODE ---------- ----------
 
-// ACTION: Force server into listening mode, making it available to respond to client requests. Note that the 
-// server will listen for requests at port 1234.
-var server = myApp.listen(process.env.PORT || 5000);
+// app.use(less(path.join(__dirname,'source','less'),{
+//     dest: path.join(__dirname, 'public'),
+//     options: {
+//         compiler: {
+//             compress: false,
+//         },
+//     },
+//     preprocess: {
+//         path: function(pathname, req) {
+//             return pathname.replace('/css/','/'); 
+//         },
+//     },
+//     force: true,
+// }));
 
-// NOTE: You can call this server using http://localhost:1234/cat_picture.jpeg
+app.listen(port, () => console.log(`Listening on port ${port}`));
